880a33b6578c208f081bb0053a8e39394d2b48a8
Jean Lucas <154537817+JeanLucasAS@users.noreply.github.com>
2025-05-21 15:59:24 -0300

    Código que puxa o cluster do mongo atlas

---
+++ main.py
@@ -0,0 +1,25 @@
+# main.py
+
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+# Conexão com o MongoDB Atlas
+client = MongoClient(
+    "mongodb+srv://admin:admin@cluster0.chxornv.mongodb.net/",
+    server_api=ServerApi('1')
+)
+
+# Selecionar o banco de dados
+db = client["test"]
+
+def mostrar_documentos(nome_colecao):
+    colecao = db[nome_colecao]
+    documentos = list(colecao.find({}))
+    
+    resultado = []
+
+    if documentos:
+        for doc in documentos:
+            item_formatado = {chave: valor for chave, valor in doc.items()}
+            resultado.append(item_formatado)
+    return resultado
